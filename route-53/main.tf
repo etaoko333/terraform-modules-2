0a0c0e8742b852638807773c23e1e9a695b5b814
etaoko333 <alongemiracle333@gmail.com>
2024-11-26 20:47:41 +0000

    created route-53 module

---
+++ route-53/main.tf
@@ -0,0 +1,17 @@
+# Get hosted zone details
+data "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name # Make sure this matches exactly
+}
+
+# Create a record set in Route 53
+resource "aws_route53_record" "site_domain" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.record_name # Specify the subdomain or root domain
+  type    = "A"
+
+  alias {
+    name                   = var.application_load_balancer.dns_name
+    zone_id                = var.application_load_balancer.zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ route-53/variables.tf
@@ -0,0 +1,5 @@
+# route-53 variables
+variable "domain_name" {}
+variable "record_name" {}
+variable "application_load_balancer.dns_name" {}
+variable "application_load_balancer.zone_id" {}

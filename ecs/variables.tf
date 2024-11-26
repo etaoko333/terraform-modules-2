86fc3e4573cc60d66b9db5476da72971e7eb7fc2
etaoko333 <alongemiracle333@gmail.com>
2024-11-26 18:18:23 +0000

    created esc-module

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -10,4 +10,4 @@ variable "region" {}
 variable "private_app_subnet_az1_id" {}
 variable "private_app_subnet_az2_id" {}
 variable "app_server_security_group_id" {}
-variable "alb_target_group.arn" {}
+variable "alb_target_group_arn" {}
